a1a63e898b64842e252ab1f6d5783bbb4bf48749
Aakash Singh <aaka241@gmail.com>
2024-05-22 15:38:57 +0530

    Added Shapes
    Added rectangle, circle and a triangle shape to the window

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -3,10 +3,12 @@
 int main() {
 
     // Define the video mode (dimensions)
-    sf::VideoMode videoMode = *(new sf::VideoMode(1920, 1080));
+    sf::VideoMode videoMode = *(new sf::VideoMode(800, 600));
 
     // Create a window object with specific dimensions and a title
     sf::RenderWindow* window = new sf::RenderWindow(videoMode, "My SFML Window");
+
+    
 
     // Game loop to keep the window open
     while (window->isOpen()) {
@@ -17,10 +19,29 @@ int main() {
                 window->close();
         }
 
+        
+
         // Clear the window
         window->clear(sf::Color::Yellow);
 
         // Draw your content here...
+        sf::CircleShape circle(50); // Radius 50
+        circle.setFillColor(sf::Color::Green);
+        circle.setPosition(400, 300); // Set position
+        window->draw(circle);
+
+        sf::RectangleShape rectangle(sf::Vector2f(70,50)); // Radius 50
+        rectangle.setFillColor(sf::Color::Red);
+        rectangle.setPosition(10,10); // Set position
+        window->draw(rectangle);
+
+        sf::ConvexShape triangle;
+        triangle.setPointCount(3);
+        triangle.setPoint(0, sf::Vector2f(200, 250));
+        triangle.setPoint(1, sf::Vector2f(250, 300));
+        triangle.setPoint(2, sf::Vector2f(300, 200));
+        triangle.setFillColor(sf::Color::Blue);
+        window->draw(triangle);
 
         // Display what was drawn
         window->display();
